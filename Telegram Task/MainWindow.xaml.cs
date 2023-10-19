c1053ff149e30d1b765f58a7666158ed3771640a
Ibrahim <alizadaibrahimm07@gmail.com>
2023-10-19 22:26:07 +0400

    Telegram Task in WPF

---
+++ Telegram Task/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Telegram_Task;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        //"Cavid Atamoghlanov"
+        //"Penahli Ibrahim"
+        //"Nuri Kenan"
+        //"Cavadova Zehra"
+        //"Musayeva Nargiz"
+        //"Alizada Ilgar"
+        //"Elgun Nesrullayev"
+        //"Sadiq Ehedov"
+    }
+}
